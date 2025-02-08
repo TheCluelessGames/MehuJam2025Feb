7f5e5b843da3544ed683ccd924a6be81d77235d6
Teemu Nokkanen <teemu.nokkanen99@gmail.com>
2025-02-08 18:34:19 +0200

    Horizontal Movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEditor.Searcher.SearcherWindow.Alignment;
+
+public class PlayerController : MonoBehaviour
+{
+    private float horizontal;
+    [SerializeField] private float playerSpeed = 7f;
+    [SerializeField] private float jumpingPower = 7.5f;
+
+    [SerializeField] private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * playerSpeed, rb.velocity.y);
+    }
+}
