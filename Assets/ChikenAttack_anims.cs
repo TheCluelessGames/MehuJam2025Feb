33f4918ebf4dbf688228faad0a204042ef030a5c
eneyl001 <eneyl001@edu.xamk.fi>
2025-02-09 22:41:00 +0200

    ending Animation trigger stuff
    script and placement things

--- Assets/ChikenAttack_anims.cs
+++ Assets/ChikenAttack_anims.cs
@@ -6,11 +6,30 @@ public class ChikenAttack_anims : MonoBehaviour
 {
     [SerializeField] Animator animatorCiken;
     [SerializeField] Animator animatorGril;
-    private Animator animator;
+    private Animator endingAnimator;
+
+    [SerializeField] Animator grannyAnim;
+
+    private bool visited = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Grandma") && !visited)
+        {
+            grannyAnim.Play("Granny_ThrowingEgg");
+        }
+    }
+
+    public void ThrowEgg () 
+    {
+        endingAnimator.Play("Ending");
+    }
+
+
     public void FightingLoop ()
     {
         animatorCiken.Play("Ciken_attack");
         animatorGril.Play("BadGril_IsAttacked");
-        animator.Play("Ending_Idle");
+        endingAnimator.Play("Ending_Idle");
     }
 }
